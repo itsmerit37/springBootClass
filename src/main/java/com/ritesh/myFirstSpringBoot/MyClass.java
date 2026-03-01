1f7a696892b6e9a9b7f9d296e67033a24913a8aa
itsmerit37 <itsmerit37@gmail.com>
2026-03-01 17:07:58 +0530

    FirstCommit

---
+++ src/main/java/com/ritesh/myFirstSpringBoot/MyClass.java
@@ -0,0 +1,12 @@
+package com.ritesh.myFirstSpringBoot;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyClass {
+    @GetMapping("abc")
+    public String sayHello(){
+        return "hello";
+    }
+}
